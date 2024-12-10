c58149da427373865d663c3af1e2eb7f133e7847
sangpham2006 <pty49990@gmail.com>
2024-12-10 15:28:09 +0700

    nộp bài session12-lession1

---
+++ b1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int tinhTong(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int so1, so2;
+
+    printf("Nhap so thu nhat: ");
+    scanf("%d", &so1);
+
+    printf("Nhap so thu hai: ");
+    scanf("%d", &so2);
+
+    int tong = tinhTong(so1, so2);
+    printf("Tong cua %d va %d la: %d\n", so1, so2, tong);
+
+    return 0;
+}
+
